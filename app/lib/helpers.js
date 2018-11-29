9ee49114ceef8cfef5975654ae16bc575cfa03b6
Akshay Kumar <mundane140@gmail.com>
2018-11-29 18:30:00 +0530

    Update helpers.js: Add methods for hashing, parsing JSON to Objects and creating random strings

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -8,6 +8,48 @@
 // Container
 var helpers = {};
 
+// Parse the JSON string to an object in all cases, without throwing
+helpers.parseJsonToObject = function(str){
+    try{
+        var object = JSON.parse(str);
+        return object;
+    } catch(e){
+        return {};
+    }
+}
+
+// Create a SHA256 hash
+helpers.hash = function(str) {
+    if(typeof(str) == 'string' && str.length > 0) {
+        var hash = crypto.createHmac('sha256', config.hashingSecret).update(str).digest('hex');
+        return hash;
+    } else {
+        return false;
+    }
+};
+
+// Create a String of random alphanumeric characters of a given length
+helpers.createRandomString = function(strLength) {
+    strLength = typeof(strLength) == 'number' && strLength > 0 ? strLength : false;
+    if(strLength){
+        // Define all possible characters that could go to the string
+        var possibleCharacter = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+        var str = '';
+        for( i = 0; i < strLength ; i ++ ){
+            // get a random character from the possibleCharacters string
+           
+            var randomCharacter = possibleCharacter.charAt(Math.floor(Math.random() * possibleCharacter.length));
+
+            // append this character to the final string
+            str += randomCharacter;
+        }
+        // Return the final string
+        return str;
+    } else {
+        return false;
+    }
+}
 
 // Exports
 module.exports = helpers;
